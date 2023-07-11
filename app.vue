0c3768442f1b05d828a41df3e191f3ac49cd30b5
dandanthemanman <85678575+dandanthemanman@users.noreply.github.com>
2023-07-11 10:52:11 -0700

    practice fetching

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <FetchResults/>
   </div>
 </template>
---
+++ components/FetchResults.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>tester</h1>
+    </div>
+</template>
+
+<script setup>
+
+// js6 fetch
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+// nuxt usefetch hook
+const { data, isFetching, error, refresh } = await useFetch("https://jsonplaceholder.typicode.com/todos/1")
+
+console.log(data.value.userId)
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
